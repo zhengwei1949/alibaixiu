5dea12f696be4fe50405cea996daa2bc37cb5161
zhengwei <zhengwei1949@qq.com>
2018-11-15 16:21:38 +0800

    动态生成分类导航

--- public/_header.php
+++ public/_header.php
@@ -1,10 +1,19 @@
+<?php
+require_once "./config.php";
+$connect = mysqli_connect(DB_HOST,DB_USER,DB_PWD,DB_NAME);//连接数据库
+$sql = "select * from categories where id != 1";
+$queryResult = mysqli_query($connect,$sql);
+$arr = [];
+while($row = mysqli_fetch_assoc($queryResult)){
+    $arr[] = $row;
+}
+?>
 <div class="header">
     <h1 class="logo"><a href="index.php"><img src="static/assets/img/logo.png" alt=""></a></h1>
     <ul class="nav">
-    <li><a href="javascript:;"><i class="fa fa-glass"></i>奇趣事</a></li>
-    <li><a href="javascript:;"><i class="fa fa-phone"></i>潮科技</a></li>
-    <li><a href="javascript:;"><i class="fa fa-fire"></i>会生活</a></li>
-    <li><a href="javascript:;"><i class="fa fa-gift"></i>美奇迹</a></li>
+    <?php foreach($arr as $key=>$value){ ?>
+    <li><a href="javascript:;"><i class="fa fa-glass"></i><?php echo $value['name'] ?></a></li>
+    <?php } ?>
     </ul>
     <div class="search">
     <form>
