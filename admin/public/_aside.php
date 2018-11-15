32856e1e98a05890c7d06f0f198befba78cffbf3
zhengwei <zhengwei1949@qq.com>
2018-11-15 17:56:33 +0800

    左侧导航高亮显示

--- admin/public/_aside.php
+++ admin/public/_aside.php
@@ -7,23 +7,23 @@
       <h3 class="name">布头儿</h3>
     </div>
     <ul class="nav">
-      <li>
+      <li class="<?php echo $current_page=='index'?'active':'' ?>">
         <a href="index.php"><i class="fa fa-dashboard"></i>仪表盘</a>
       </li>
-      <li class="active">
+      <li>
         <a href="#menu-posts" class="collapsed" data-toggle="collapse">
           <i class="fa fa-thumb-tack"></i>文章<i class="fa fa-angle-right"></i>
         </a>
         <ul id="menu-posts" class="collapse">
-          <li><a href="posts.php">所有文章</a></li>
-          <li><a href="post-add.php">写文章</a></li>
-          <li class="active"><a href="categories.php">分类目录</a></li>
+          <li class="<?php echo $current_page=='posts'?'active':'' ?>"><a href="posts.php">所有文章</a></li>
+          <li class="<?php echo $current_page=='post-add'?'active':'' ?>"><a href="post-add.php">写文章</a></li>
+          <li class="<?php echo $current_page=='categories'?'active':'' ?>"><a href="categories.php">分类目录</a></li>
         </ul>
       </li>
-      <li>
+      <li class="<?php echo $current_page=='comments'?'active':'' ?>">
         <a href="comments.php"><i class="fa fa-comments"></i>评论</a>
       </li>
-      <li>
+      <li class="<?php echo $current_page=='users'?'active':'' ?>">
         <a href="users.php"><i class="fa fa-users"></i>用户</a>
       </li>
       <li>
@@ -31,9 +31,9 @@
           <i class="fa fa-cogs"></i>设置<i class="fa fa-angle-right"></i>
         </a>
         <ul id="menu-settings" class="collapse">
-          <li><a href="nav-menus.php">导航菜单</a></li>
-          <li><a href="slides.php">图片轮播</a></li>
-          <li><a href="settings.php">网站设置</a></li>
+          <li class="<?php echo $current_page=='nav-menus'?'active':'' ?>"><a href="nav-menus.php">导航菜单</a></li>
+          <li class="<?php echo $current_page=='slides'?'active':'' ?>"><a href="slides.php">图片轮播</a></li>
+          <li class="<?php echo $current_page=='settings'?'active':'' ?>"><a href="settings.php">网站设置</a></li>
         </ul>
       </li>
     </ul>
