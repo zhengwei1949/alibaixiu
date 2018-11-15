3d6051d911da3fd5c837bb7f293e543559a2add7
zhengwei <zhengwei1949@qq.com>
2018-11-15 17:59:39 +0800

    文章展开隐藏功能

--- admin/public/_aside.php
+++ admin/public/_aside.php
@@ -11,10 +11,11 @@
         <a href="index.php"><i class="fa fa-dashboard"></i>仪表盘</a>
       </li>
       <li>
-        <a href="#menu-posts" class="collapsed" data-toggle="collapse">
+        <?php $postArr = ['posts','post-add','categories'] ?>
+        <a href="#menu-posts" class="<?php echo in_array($current_page,$postArr)?'':'collapsed' ?>" data-toggle="collapse">
           <i class="fa fa-thumb-tack"></i>文章<i class="fa fa-angle-right"></i>
         </a>
-        <ul id="menu-posts" class="collapse">
+        <ul id="menu-posts" class="collapse <?php echo in_array($current_page,$postArr)?'in':'' ?>">
           <li class="<?php echo $current_page=='posts'?'active':'' ?>"><a href="posts.php">所有文章</a></li>
           <li class="<?php echo $current_page=='post-add'?'active':'' ?>"><a href="post-add.php">写文章</a></li>
           <li class="<?php echo $current_page=='categories'?'active':'' ?>"><a href="categories.php">分类目录</a></li>
